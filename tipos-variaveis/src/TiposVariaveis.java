25a220f3a29b71e67f99e07574e172506a31ac31
Marcus Vinicius <marcusvrbacelar@gmail.com>
2025-05-05 22:06:10 -0300

    Variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,27 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500.33;
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 70658196;
+        long cpf = 546465563L;
+        float pi = 3.14F;
+        double salario = 2100.33; 
+
+
+
+       //  short numeroCurto = 1;
+       //  int numeroNormal = numeroCurto;
+       //  short numeroCurto2 = numeroNormal; // linguagem estramanete tipada, por isso o erro.
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short)numeroNormal; // correção do erro.
+
+
+       final int NUMERO = 1; // Fala q a variavel e absoluta, náo pode mudar o valor. OBS: Escrever em caixa alta.
+
+        System.out.println(NUMERO);
+
     }
 }
